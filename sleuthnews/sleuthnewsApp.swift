932205dff460b192ca1e84e6ecc8f25e5845b527
Mitja Felicijan <mitja.felicijan@gmail.com>
2023-05-16 18:19:00 +0200

    Initial Commit

---
+++ sleuthnews/sleuthnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleuthnewsApp.swift
+//  sleuthnews
+//
+//  Created by Mitja Felicijan on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct sleuthnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
